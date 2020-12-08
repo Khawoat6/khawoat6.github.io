efbded0210929a7b10b3da1470c562f16aee3b6c
oat phattaraphon <phattaraphon.c@ku.th>
2020-12-09 00:09:48 +0700

    deploy on gh-page

--- src/App.js
+++ src/App.js
@@ -1,58 +1,36 @@
-/* function App() {
-  return (
-    <div className="App">
-      <h1>Hello Oat Phattaraphon</h1>
-    </div>
-  );
-}
-
-export default App; */
-
 /* import styled from 'styled-components'; */
-import React from "react";
+import React from 'react';
 
 // IMPORT COMPONENTS
-import Navbar from "./components/Navbar";
+import Navbar from './components/Navbar';
 //import Section from "./components/Section";
-import About from "./components/Section";
-import Hero from "./components/sections/Hero";
+import About from './components/Section';
+import Hero from './components/sections/Hero';
 
-import dummyText from "./components/DummyText";
+import dummyText from './components/DummyText';
+import Side from './components/Side';
 
 // IMPORT STYLE
 import './assets/sass/_main.scss';
 
 function App() {
-    return (
-      <div className="App">
-        <Navbar />
-        <Hero />
-        <About
-          title="About"
-          subtitle={dummyText}
-          dark={true}
-          id="about"
-        />
-        <About
-          title="Experience"
-          subtitle={dummyText}
-          dark={false}
-          id="experience"
-        />
-        <About
-          title="Work"
-          subtitle={dummyText}
-          dark={true}
-          id="work"
-        />
-        <About
-          title="Contact"
-          subtitle={dummyText}
-          dark={false}
-          id="contact"
-        />
-      </div>
-    );
+  return (
+    <div className="App">
+      <Navbar />
+      <Hero />
+      <Side />
+
+      <About title="About" subtitle={dummyText} dark={true} id="about" />
+      <About
+        title="Experience"
+        subtitle={dummyText}
+        dark={false}
+        id="experience"
+      />
+      <About title="Work" subtitle={dummyText} dark={true} id="work" />
+      <About title="Contact" subtitle={dummyText} dark={false} id="contact" />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Side.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Side() {
+    return (
+        <>
+        <div className="side">
+            <div className="mailto">
+               <a href="mailto:phattaraphon.c@ku.th">phattaraphon.c@ku.th</a>
+            </div>
+            <div>
+            <hr
+            style={{
+                color: '#6D7F99',
+                backgroundColor: '#6D7F99',
+                height: 0.2,
+                borderColor: '#6D7F99',
+                }}
+                />
+            </div>
+        </div>
+        </>
+    );
+}
+
+export default Side;
